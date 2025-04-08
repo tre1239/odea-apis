ec320f101cb959f181d23cf0db2e47767407d3aa
Connor Easton <eastonco@icloud.com>
2025-04-08 08:35:30 -0700

    Create exercise.js

---
+++ exercise.js
@@ -0,0 +1,93 @@
+// Numbers and Math
+
+function add(a, b) {
+  // Return the sum of two numbers
+}
+
+function square(n) {
+  // Return the square of a number
+}
+
+function evenOrOdd(n) {
+  // Return "even" if n is even, otherwise "odd"
+}
+
+function maxOfTwo(a, b) {
+  // Return the larger of two numbers
+}
+
+function multiplyList(numbers) {
+  // Multiply all numbers in a list and return the result
+}
+
+
+
+// Strings
+
+function greet(name) {
+  // Return "Hello, <name>!"
+}
+
+function countLetters(word) {
+  // Return the number of letters in the word
+}
+
+function makeUppercase(s) {
+  // Return the string in all uppercase letters
+}
+
+function repeatWord(word, times) {
+  // Return the word repeated 'times' times
+}
+
+function firstAndLast(s) {
+  // Return the first and last character of the string
+}
+
+
+
+// Lists
+
+function firstItem(lst) {
+  // Return the first item in the list
+}
+
+function lastItem(lst) {
+  // Return the last item in the list
+}
+
+function sumList(lst) {
+  // Return the sum of all numbers in the list
+}
+
+function listLength(lst) {
+  // Return how many items are in the list
+}
+
+function containsItem(lst, item) {
+  // Return true if item is in the list
+}
+
+
+
+// Logic and Conditions
+
+function isPositive(n) {
+  // Return true if n is positive
+}
+
+function canVote(age) {
+  // Return true if age is 18 or older
+}
+
+function isWeekend(day) {
+  // Return true if day is "Saturday" or "Sunday"
+}
+
+function isEmpty(s) {
+  // Return true if the string is empty
+}
+
+function sameNumber(a, b) {
+  // Return true if a and b are the same number
+}
